1ce1fb599572808eb9c296100038e4592d3422ab
vbn930 <vbn930@naver.com>
2025-05-16 17:19:23 +0900

    Genrate ASP .NET Core Project

---
+++ ExchangeRateNotifierWorkerService/Program.cs
@@ -0,0 +1,7 @@
+using ExchangeRateNotifierWorkerService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
