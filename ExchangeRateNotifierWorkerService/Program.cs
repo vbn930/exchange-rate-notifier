f8d613b96fd844ba2d6e231327494fd72fcd70ef
vbn930 <vbn930@naver.com>
2025-06-02 17:09:09 +0900

    Remove test code

--- ExchangeRateNotifierWorkerService/Program.cs
+++ ExchangeRateNotifierWorkerService/Program.cs
@@ -10,29 +10,5 @@ using System.Text.Json.Serialization;
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
-//Test code for api request
-const string API_KEY = "c511651c1c924ddc9b621f261b3ee649";
-string url = $"https://openexchangerates.org/api/latest.json?app_id={API_KEY}&base=USD&prettyprint=true&show_alternative=false";
-
-var httpClient = new HttpClient();
-var apiRequestWrapper = new APIRequestWrapper(httpClient);
-var exchangeApiClient = new ExchangeAPIClient(apiRequestWrapper, API_KEY);
-
-var data = await exchangeApiClient.GetExchangeRateDataAsync();
-
-if (data != null)
-{
-    Console.WriteLine($"Timestamp: {data.Timestamp}");
-    Console.WriteLine($"Rate: 1USD->{data.Rates["KRW"]}₩");
-
-    var eur = CurrencyConvertor.ConvertToKRW(data, "EUR");
-    Console.WriteLine($"Rate: 1EUR->{eur}₩");
-}
-else
-{
-    Console.WriteLine("API Result is NULL");
-}
-//test code ends
-
 var host = builder.Build();
 host.Run();
